2b2d025b6919ae322479a53fc60607c100c535ee
Austin Aitken <alaitken@users.noreply.github.com>
2020-02-29 14:52:07 -0330

    Created the main player move and look controls

---
+++ Assets/PlayerLook.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    [SerializeField] float sensitivity = 100f;
+    [SerializeField] float maxAngle = 90f;
+    [SerializeField] Transform player;
+
+    private float xRot = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked; //Locks cursor to center view
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        player.Rotate(Vector3.up * mouseX); //Rotates about the y-axis to look left and right
+
+        xRot -= mouseY; //updates the rotation on the x-axis based on the mouse input on the y-axis
+        xRot = Mathf.Clamp(xRot, -maxAngle, maxAngle); //restricts the rotation to our maxAngle;
+
+        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
+        player.Rotate(Vector3.up * mouseX);
+    }
+
+   // private void 
+}
---
+++ Assets/PlayerWalking.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWalking : MonoBehaviour
+{
+
+    CharacterController characterController;
+    [SerializeField] float speed = 1f;
+    [SerializeField] float gravity = 9.81f;
+
+    private Vector3 moveDirection = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveCharacter();
+    }
+
+    private void MoveCharacter()
+    {
+        if (characterController.isGrounded)
+        {
+            float x = Input.GetAxis("Horizontal");
+            float z = Input.GetAxis("Vertical");
+            moveDirection = (transform.right * x) + (transform.forward * z);
+            moveDirection *= speed;
+        }
+
+        moveDirection.y -= gravity * Time.deltaTime;
+
+        characterController.Move(moveDirection * Time.deltaTime);
+    }
+   
+    
+}
