55ff24ab82222fdf01e9dc4948105e500e1bdcc4
Austin Aitken <austin.aitken@hotmail.com>
2020-03-12 18:20:01 -0400

    Started Main Menu, PlayerChoose, and fixed doors

---
+++ Assets/Scripts/PickupWallet.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupWallet : MonoBehaviour
+{
+    [SerializeField] bool isPickedup = false;
+    private bool isInRange = false;
+    [SerializeField] GameObject wallet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        WalletPickup();
+    }
+
+    private void WalletPickup()
+    {
+        if (isInRange && !isPickedup && Input.GetKey(KeyCode.E))
+        {
+            isPickedup = true;
+            Destroy(wallet);
+        }
+    }
+
+    private void OnTriggerEnter(Collider collide)
+    {
+        if (collide.tag == "Wallet")
+        { 
+            isInRange = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider collide)
+    {
+        if (collide.tag == "Wallet")
+        {
+            isInRange = false;
+        }
+    }
+}
--- Assets/Scripts/PlayerChooseOption.cs
+++ Assets/Scripts/PlayerChooseOption.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerChooseOption : MonoBehaviour
 {
@@ -9,18 +10,20 @@ public class PlayerChooseOption : MonoBehaviour
     [SerializeField] GameObject answerOne;
     [SerializeField] GameObject answerTwo;
     [SerializeField] GameObject answerThree;
-
+    private Transform playerTransform;
+    private bool hasInteracted = false;
     private bool isInRange = false;
 
     void Start()
     {
-        
+        playerTransform = GameObject.Find("MainPlayer").GetComponent<Transform>();
+        GameObject.Find("Canvas").GetComponent<Canvas>().enabled = false;
     }
 
     private void OnTriggerEnter(Collider collide)
     {
         if (collide.tag == "Player")
-        {
+        { 
             isInRange = true;
         }
     }
@@ -35,9 +38,26 @@ public class PlayerChooseOption : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (isInRange)
+        if (isInRange && !hasInteracted)
         {
-
+            Cursor.lockState = CursorLockMode.Confined;
+            GameObject.Find("MainPlayer").GetComponent<PlayerWalking>().enabled = false;
+            GameObject.Find("Camera").GetComponent<PlayerLook>().enabled = false;
+            playerTransform.LookAt(gameObject.transform);
+            transform.LookAt(playerTransform);
+            GameObject.Find("Canvas").GetComponent<Canvas>().enabled = true;
+            if (Input.GetKey(KeyCode.E))
+            {
+                GameObject.Find("Canvas").GetComponent<Canvas>().enabled = false;
+                playerTransform.SetPositionAndRotation(playerTransform.position, new Quaternion(0,0,0,0));
+                hasInteracted = true;
+            }
+        }
+        else
+        {
+            //Cursor.lockState = CursorLockMode.Locked;
+            GameObject.Find("MainPlayer").GetComponent<PlayerWalking>().enabled = true;
+            GameObject.Find("Camera").GetComponent<PlayerLook>().enabled = true;
         }
     }
 }
