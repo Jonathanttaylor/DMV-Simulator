0e74a179481814db0071b07a4bfbc215b39af7ea
Jonathan Taylor <jttaylor@mun.ca>
2020-03-18 21:25:23 -0230

    Add bus movement

--- Assets/Scripts/BusSitting.cs
+++ Assets/Scripts/BusSitting.cs
@@ -1,14 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class Sitting : MonoBehaviour
+public class BusSitting : MonoBehaviour
 {
-    private PlayerLook look;
-
+    // Creating Seralized Fields and private members
     [SerializeField] Transform animationStart;
-    private GameObject player;
     private Animator animator;
+    private PlayerLook look;
+    private BusMovement move;
+    private GameObject player;
     private bool isInRange;
     private bool isSitting;
 
@@ -17,6 +19,7 @@ public class Sitting : MonoBehaviour
     void Start()
     {
         look = FindObjectOfType(typeof(PlayerLook)) as PlayerLook;
+        move = FindObjectOfType(typeof(BusMovement)) as BusMovement;
 
         player = GameObject.FindGameObjectWithTag("Player");
         animator = player.GetComponent<Animator>();
@@ -24,7 +27,7 @@ public class Sitting : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.CompareTag("Player"))
         {
             isInRange = true;
         }
@@ -32,7 +35,7 @@ public class Sitting : MonoBehaviour
 
     private void OnTriggerExit(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.CompareTag("Player"))
         {
             isInRange = false;
         }
@@ -56,22 +59,34 @@ public class Sitting : MonoBehaviour
         {
             if (Input.GetKeyDown(KeyCode.E))
             {
+                // Moves player infront of chair and starts sitting animation
                 player.transform.position = animationStart.position;
                 player.transform.rotation = animationStart.rotation;
-
                 animator.SetBool("sit", true);
+
+                // Disables "W" "A" "S" "D" controls. Locks camera at 90 on x and y
                 player.GetComponent<CharacterController>().enabled = false;
                 player.transform.Find("Camera").GetComponent<PlayerLook>().enabled = false;
+
                 isSitting = true;
+
+                player.transform.parent = move.transform; // Makes player object a child of Bus object
+                move.StartDriving(); // Calls BusMovement Script to start driving the bus
             }
         }
 
-        if (isSitting && Input.GetKeyDown(KeyCode.W))
+        if (move.GetIsBraking() && move.GetCurrentSpeed() > -0.1 && Input.GetKeyDown(KeyCode.W))
         {
-            animator.SetBool("sit", false);
+            animator.SetBool("sit", false); // Starts standing animation
+
+            // Resets player controls to normal 
             player.GetComponent<CharacterController>().enabled = true;
             player.transform.Find("Camera").GetComponent<PlayerLook>().enabled = true;
+
             isSitting = false;
+
+            player.transform.parent = null; // Makes player object no longer child of Bus object
+            SceneManager.MoveGameObjectToScene(player, SceneManager.GetSceneByName("MainPlayer")); // Moves player object back inside MainPlayer scene
         }
     }
 }
