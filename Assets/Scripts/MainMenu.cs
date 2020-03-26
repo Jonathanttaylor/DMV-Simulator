8c20848e75d2a30c6d16ef678c0bcc312274bfad
Austin Aitken <alaitken@mun.ca>
2020-03-25 23:02:40 -0230

    Fixed player interaction and main menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+        SceneManager.LoadScene(2, LoadSceneMode.Additive);
+        SceneManager.LoadScene(3, LoadSceneMode.Additive);
+        SceneManager.LoadScene(4, LoadSceneMode.Additive);
+        SceneManager.LoadScene(5, LoadSceneMode.Additive);
+        SceneManager.LoadScene(6, LoadSceneMode.Additive);
+        SceneManager.LoadScene(7, LoadSceneMode.Additive);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
