e4e3804be39644924581cc1aa18ae74d39fc9d9e
Jonathan Taylor <jttaylor@mun.ca>
2020-04-01 16:34:52 -0230

    Set sidewalk/road read/write enable true

---
+++ Assets/Scripts/ComputerSitting.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ComputerSitting : MonoBehaviour
+{
+    [SerializeField] GameObject computer;
+    [SerializeField] Transform animationStart;
+    private Animator animator;
+    private bool isSitting;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = computer.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        InteractSeat();
+    }
+
+    void InteractSeat()
+    {
+    }
+}
