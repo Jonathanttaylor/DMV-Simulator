9c8bdcf2a03eb4def18743031e56a1c710ad2732
Austin Aitken <alaitken@mun.ca>
2020-04-01 20:48:42 -0230

    Torture Ending

---
+++ Assets/Scripts/PlayAmy.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayAmy : MonoBehaviour
+{
+    [SerializeField] AudioClip badComedians;
+    private AudioSource audioSource;
+    private bool isPlaying = false;
+    public bool start = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playAmy();
+    }
+
+    private void playAmy()
+    {
+        if (!isPlaying && start)
+        {
+            audioSource.PlayOneShot(badComedians);
+            isPlaying = true;
+        }
+    }
+}
