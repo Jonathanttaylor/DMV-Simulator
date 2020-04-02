e445c488bc5373b711637a3e23284765a78980dc
Jonathan Taylor <jttaylor@mun.ca>
2020-04-02 07:15:13 -0230

    Fix Merge Conflicts

---
+++ Assets/Scripts/Clock.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Clock : MonoBehaviour
+{
+    private float timeF;
+    private int timeI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeF = (Time.time) / 60;
+        timeI = Mathf.FloorToInt(timeF) + 15;
+
+        if (timeI == 60)
+        {
+            transform.GetComponent<TextMesh>().text = "3:00";
+        }
+        else
+        {
+            transform.GetComponent<TextMesh>().text = "2:" + timeI;
+        }
+    }
+}
--- Assets/Scripts/DMVGuyMovement.cs
+++ Assets/Scripts/DMVGuyMovement.cs
@@ -6,11 +6,14 @@ using UnityEngine.AI;
 public class DMVGuyMovement : MonoBehaviour
 {
     [SerializeField] Transform waypoint;
+    [SerializeField] Transform waypoint2;
+    [SerializeField] Transform waypoint3;
+    [SerializeField] Transform waypoint4;
     private NavMeshAgent navMeshAgent;
     private BusMovement bus;
+    private DMVGuyBusDialogue1 dialogue;
     private bool moved;
     private float timer = 4f;
-    private bool sit;
     //private float rotationSpeed = 10f;
     private bool isSitting;
 
@@ -21,45 +24,82 @@ public class DMVGuyMovement : MonoBehaviour
     {
         navMeshAgent = this.GetComponent<NavMeshAgent>();
         bus = FindObjectOfType(typeof(BusMovement)) as BusMovement;
+        dialogue = FindObjectOfType(typeof(DMVGuyBusDialogue1)) as DMVGuyBusDialogue1;
         animator = GetComponent<Animator>();
         moved = false;
-        sit = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Vector3 vectorToWaypoint = waypoint2.position;
+            navMeshAgent.SetDestination(vectorToWaypoint);
+        }
+
         if (bus.ReturnStop() == "stop1" && bus.GetCurrentSpeed() > -0.1 && !moved)
         {
             timer -= 1 * Time.deltaTime;
 
             if (timer < 0.1f)
             {
-                Move();
+                Move(waypoint);
             }
         }
 
-        if (IsInRangeOfChair())
+        if (bus.ReturnStop() == "stop2")
+        {
+            moved = false;
+            timer = 4f;
+        }
+
+        if (bus.ReturnStop() == "stop5" && bus.GetCurrentSpeed() > -0.1 && !moved && dialogue.ReturnIsPressed())
+        {
+            Debug.Log("In DMVGuy Should be moving");
+
+            animator.SetBool("sit", false);
+            transform.parent = null;
+            transform.position = waypoint3.position;
+            GetComponent<NavMeshAgent>().enabled = true;
+
+            timer -= 1 * Time.deltaTime;
+
+            if (timer < 0.1f)
+            {
+                Move(waypoint2);
+            }
+        }
+
+        if (IsInRangeOf(waypoint2))
+        {
+            GetComponent<NavMeshAgent>().enabled = false;
+            transform.position = waypoint4.position;
+        }
+
+        if (IsInRangeOf(waypoint))
         {
             Sit();
         }
     }
 
-    private void Move()
+    private void Move(Transform target)
     {
-        Vector3 vectorToWaypoint = waypoint.position;
+        Vector3 vectorToWaypoint = target.position;
         navMeshAgent.SetDestination(vectorToWaypoint);
         moved = true;
     }
 
-    private bool IsInRangeOfChair()
+    private bool IsInRangeOf(Transform target)
     {
-        float distance = Vector3.Distance(transform.position, waypoint.position);
-        return distance < 0.5f;
+        Debug.Log("CheckSit");
+        float distance = Vector3.Distance(transform.position, target.position);
+        return distance < 1f;
     }
 
     private void Sit()
     {
+        Debug.Log("Sit");
         //Vector3 direction = (target.position - transform.position).normalized;
         //Quaternion lookRotation = Quaternion.LookRotation(direction);
         //transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
