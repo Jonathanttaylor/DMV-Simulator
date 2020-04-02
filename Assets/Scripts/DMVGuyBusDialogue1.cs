5a1a88a17ca591ae9445bd363a713950f81b8c0f
Austin Aitken <alaitken@mun.ca>
2020-04-02 21:03:29 -0230

    Finished the city and fixed head

--- Assets/Scripts/DMVGuyBusDialogue1.cs
+++ Assets/Scripts/DMVGuyBusDialogue1.cs
@@ -83,7 +83,7 @@ public class DMVGuyBusDialogue1 : MonoBehaviour
                 // player.transform.rotation = Quaternion.Slerp(player.transform.rotation, targetRotationPlayer, speed * Time.deltaTime);
                 //  transform.rotation = Quaternion.Slerp(transform.rotation, targetRotationNPC, speed * Time.deltaTime);
                 playerCamera.transform.LookAt(transform);
-                transform.Rotate(60, 0, 0);
+                transform.Rotate(-30, 0, 0);
                 isLookAt = true;
             }
         }
